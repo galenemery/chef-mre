9062493161c94f02eed156eeba3b76ace3717bb3
John Snow <thelunaticscripter@outlook.com>
2016-10-29 06:43:21 -0700

    Add empty recipes for artifactory install

---
+++ recipes/artifactory.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef-mre
+# Recipe:: artifactory
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
---
+++ recipes/provision-artifactory.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef-mre
+# Recipe:: provision-artifactory
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
